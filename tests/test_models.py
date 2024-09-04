89312274eae236fceb5b6339cc653d1fdfa385c4
K <me_oh_yeah@hotmail.com>
2024-09-03 21:47:38 -0400

    fix syntax

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,12 +3,13 @@ from src.pytemplate.domain.models import TrafficLightState
 
 # eval all possible enumeration values
 def test_enum_values():
-  assert TrafficLightState.RED.value == "RED"
-  assert TrafficLightState.YELLOW.value == "YELLOW"
-  assert TrafficLightState.GREEN.value == "GREEN"
-
+    assert TrafficLightState.RED.value == "RED"
+    assert TrafficLightState.YELLOW.value == "YELLOW"
+    assert TrafficLightState.GREEN.value == "GREEN"
+    
 # eval all possible enum strings
 def test_enum_str_method():
-  assert str(TrafficLightState.RED) == "RED"
-  assert str(TrafficLightState.YELLOW) == "YELLOW"
-  assert str(TrafficLightState.GREEN) == "GREEN"
+    assert str(TrafficLightState.RED) == "RED"
+    assert str(TrafficLightState.YELLOW) == "YELLOW"
+    assert str(TrafficLightState.GREEN) == "GREEN"
+    
