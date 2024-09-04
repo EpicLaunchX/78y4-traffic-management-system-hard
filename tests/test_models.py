d781dc6ab1c811e85042345bec8baa323cf93d34
K <me_oh_yeah@hotmail.com>
2024-09-03 21:16:21 -0400

    fix test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,13 @@
 # tests/test_models.py
 from src.pytemplate.domain.models import TrafficLightState
 
+# eval all possible enumeration values
 def test_enum_values():
   assert TrafficLightState.RED.value == "RED"
   assert TrafficLightState.YELLOW.value == "YELLOW"
   assert TrafficLightState.GREEN.value == "GREEN"
 
+# eval all possible enum strings
 def test_enum_str_method():
   assert str(TrafficLightState.RED) == "RED"
   assert str(TrafficLightState.YELLOW) == "YELLOW"
