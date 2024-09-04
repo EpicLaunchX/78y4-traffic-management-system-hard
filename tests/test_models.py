2b0b1ea5b1d1c711966ac776f8b7efe047779ed6
K <me_oh_yeah@hotmail.com>
2024-09-03 22:44:10 -0400

    fix syntax

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,10 +6,9 @@ def test_enum_values():
     assert TrafficLightState.RED.value == "RED"
     assert TrafficLightState.YELLOW.value == "YELLOW"
     assert TrafficLightState.GREEN.value == "GREEN"
-    
+
 # eval all possible enum strings
 def test_enum_str_method():
     assert str(TrafficLightState.RED) == "RED"
     assert str(TrafficLightState.YELLOW) == "YELLOW"
     assert str(TrafficLightState.GREEN) == "GREEN"
-    
